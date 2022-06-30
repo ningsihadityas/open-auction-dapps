8351ae9e68976e406922c54cf9bb2e2ae301fa5c
adityas <adityas@192.168.1.10>
2022-07-01 01:40:30 +0700

    migrate multiple contract

--- backend/migrations/1_initial_migration.js
+++ backend/migrations/1_initial_migration.js
@@ -3,23 +3,24 @@ const MainAuction = artifacts.require('MainAuction');
 const Bidding = artifacts.require('Bidding');
 
 module.exports = function (deployer) {
-  let auctionId;
-  let assetName;
-  let assetDetail;
-  let startPrice;
-  let assetOwner;
-  let ownerDeposite;
-  let auctionDuration;
+  let auctionId = 0;
+  let assetName = '';
+  let assetDetail = '';
+  let startPrice = 0;
+  let assetOwner = '0x0000000000000000000000000000000000000000';
+  let ownerDeposite = 0;
+  let auctionDuration = 0;
 
-  deployer.deploy(MainAuction);
-  deployer.deploy(
-    Bidding,
-    auctionId,
-    assetName,
-    assetDetail,
-    startPrice,
-    assetOwner,
-    ownerDeposite,
-    auctionDuration
-  );
+  deployer.deploy(MainAuction).then(function () {
+    return deployer.deploy(
+      Bidding,
+      auctionId,
+      assetName,
+      assetDetail,
+      startPrice,
+      assetOwner,
+      ownerDeposite,
+      auctionDuration
+    );
+  });
 };
